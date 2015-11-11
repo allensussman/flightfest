fe744585f3c2078abe0752eb1fd4348ec1b4cef2
Allen Sussman <allen.sussman@sojern.com>
2015-11-10 18:45:41 -0800

    Add min ticket price to ticket link.

--- flightfest/flightfest.py
+++ flightfest/flightfest.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import render_template
 from constants import ORIGIN, STUBHUB_BASE_URL
 from api_calls import get_events, get_listings, get_flights
+from collections import Counter
 
 app = Flask(__name__)
 
@@ -27,46 +28,75 @@ def get_and_show_results():
     for idx, event in enumerate(events):
         params_dict['lat{}'.format(idx+1)] = event['venue']['latitude']
         params_dict['long{}'.format(idx+1)] = event['venue']['longitude']
+        params_dict['description{}'.format(idx+1)] = popup_content(event)
 
-        performers = event.get('performers')
-        if performers:
-            name = performers[0]['name']
-        else:
-            performers_collection = event.get('performersCollection')
-            if performers_collection:
-                name = performers_collection[0]['name']
-            else:
-                name = event['name']
-
-        venue = event['displayAttributes']['primaryName']
-
-        venue_dict = event['venue']
-        city, state, country = venue_dict['city'], venue_dict['state'], venue_dict['country']
-        if country == 'US':
-            geo_parts = [city, state]
-        else:
-            geo_parts = [city, country]
-        geo_string = ', '.join(geo_parts)
-
-        ticket_url = ''.join([STUBHUB_BASE_URL, event['eventUrl']])
-        ticket_link = """<a href="{}">Buy show tickets<\\a>""".format(ticket_url)
-
-        date = event['eventDateLocal']
-        date_str = '/'.join([date[5:7], date[8:10], date[:4]])
-
-        description = ' <br> '.join([name, venue, geo_string, date_str, ticket_link])
-        params_dict['description{}'.format(idx+1)] = description
-
-        print get_listings(event['id'])
+        # # get min ticket price
+        # import json
+        # with open('listing.json', 'w') as f:
+        #     json.dump(get_listings(event['id']), f)
 
         # for date, city in zip(dates, cities):
         #     get_emirates_results(date, ORIGIN, city, 'Economy')
     return render_template("results.html", **params_dict)
 
 
-@app.route('/slides.html')
-def render_slides_page():
-    return render_template("slides.html")
+def popup_content(event):
+    # Set event name
+    performers = event.get('performers')
+    if performers:
+        name = performers[0]['name']
+    else:
+        performers_collection = event.get('performersCollection')
+        if performers_collection:
+            name = performers_collection[0]['name']
+        else:
+            name = event['name']
+
+    # Set venue name
+    venue = event['displayAttributes']['primaryName']
+
+    # Set location string
+    venue_dict = event['venue']
+    city, state, country = venue_dict['city'], venue_dict['state'], venue_dict['country']
+    if country == 'US':
+        geo_parts = [city, state]
+    else:
+        geo_parts = [city, country]
+    geo_string = ', '.join(geo_parts)
+
+    # Set ticket link
+    tlink = ticket_link(event)
+
+    # Set date string
+    date = event['eventDateLocal']
+    date_str = '/'.join([date[5:7], date[8:10], date[:4]])
+
+    # Build popup content
+    content = ' <br> '.join([name, venue, geo_string, date_str, tlink])
+    return content
+
+
+def ticket_link(event):
+    # Set ticket url
+    ticket_url = ''.join([STUBHUB_BASE_URL, event['eventUrl']])
+
+    # Set ticket link text
+    listings_dict = get_listings(event['id'])
+    listings = listings_dict['listing']
+
+    if listings:
+        currencies = [listing['currentPrice']['currency'] for listing in listings]
+        most_common_currency = Counter(currencies).most_common()[0][0]
+        min_price = min([listing['currentPrice']['amount'] for listing in listings
+                         if listing['currentPrice']['currency'] == most_common_currency])
+
+        link_text = "Tickets from {0:.2f} {1:s} (includes fees)".format(min_price,
+                                                                     most_common_currency)
+    else:
+        link_text = "No tickets currently on StubHub"
+
+    return """<a href="{}">{}<\\a>""".format(ticket_url, link_text)
+
 
 if __name__ == '__main__':
     app.run("0.0.0.0", port=5000)
