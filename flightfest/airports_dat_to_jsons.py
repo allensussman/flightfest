0b1d3dafddcbc23950fd7c3206c1fa7dfd153678
Allen Sussman <allen.sussman@sojern.com>
2015-11-23 22:12:39 -0800

    Comments, pep8, airports_dat_to_json.py description, delete unused files.

--- flightfest/airports_dat_to_jsons.py
+++ flightfest/airports_dat_to_jsons.py
@@ -7,10 +7,14 @@ from constants import AIRPORT_LAT_LONG_FILE, AIRPORT_TIMEZONE_FILE
 
 DEFAULT_AIRPORT_DAT = 'airports.dat'
 HELP_TEMPLATE = '{} (default: {})'
+DESCRIPTION = 'From OpenFlights\' airport.dat (http://openflights.org/data.html), create (1) a ' \
+              'JSON file with major airports and their lat/longs and (2) a JSON file with ' \
+              'airports and their timezone, in hours after UTC time.'
 
 
 def output_lat_long_file(airport_data_file, lat_long_file):
     with open(airport_data_file) as f:
+        # Don't include airports with \N for ICAO code or with 'Muni' (for municipal) in the name
         lat_longs = [{'code': row[4], 'lat': float(row[6]), 'long': float(row[7])}
                      for row in csv.reader(f) if row[4] and row[5] != '\N' and 'Muni' not in row[1]]
     output_json(lat_longs, lat_long_file)
@@ -28,7 +32,7 @@ def output_json(dictionary, filename):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Input Parameters')
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument("--input_file", "-i", dest="input_file", default=DEFAULT_AIRPORT_DAT,
                         help=HELP_TEMPLATE.format('Input airport data file', DEFAULT_AIRPORT_DAT))
     parser.add_argument("--output_lat_long_file", "-l", dest="lat_long_file",
